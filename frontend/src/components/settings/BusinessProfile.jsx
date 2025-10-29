9f550ab1938d3fa36dffe3939217d47aa605dd2a
IanGorski <gorskiandevl@gmail.com>
2025-10-28 21:52:54 -0300

    feat: agregar componentes de configuración (BusinessProfile, UserManagement, etc.)

---
+++ frontend/src/components/settings/BusinessProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BusinessProfile = () => {
+    return (
+        <div>
+            {/* Business Profile Component */}
+        </div>
+    );
+};
+
+export default BusinessProfile;
---
+++ frontend/src/components/settings/PaymentMethods.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PaymentMethods = () => {
+    return (
+        <div>
+            {/* Payment Methods Component */}
+        </div>
+    );
+};
+
+export default PaymentMethods;
