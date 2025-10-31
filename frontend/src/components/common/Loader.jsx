3ffdb358be1e493fa62a2f22b09882fb25db2335
IanGorski <gorskiandevl@gmail.com>
2025-10-31 02:30:22 -0300

    style: migrar estilos inline a Loader.css y ajustar Loader.jsx

--- frontend/src/components/common/Loader.jsx
+++ frontend/src/components/common/Loader.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import './Loader.css';
 
 const Loader = () => {
   return (
-    <div className="flex items-center justify-center h-full">
-      <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    <div className="loader-container">
+      <div className="loader"></div>
     </div>
   );
 };
