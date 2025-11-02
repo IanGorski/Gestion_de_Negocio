6924bdc5882dc12358caabd14616bdcc00155f2c
IanGorski <gorskiandevl@gmail.com>
2025-11-01 22:24:20 -0300

    feat(common): actualizar componente Navbar

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,49 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiMenu, FiX } from 'react-icons/fi';
-import { useAuthContext } from '../../context/AuthContext';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiMenu, FiX } from "react-icons/fi";
+import "./Navbar.css";
 
 const Navbar = ({ onToggleSidebar, isSidebarOpen }) => {
-    const { user, logout } = useAuthContext();
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* Left Section */}
+        <div className="navbar-left">
+          <button
+            onClick={onToggleSidebar}
+            className="navbar-toggle"
+            aria-label="Toggle menu"
+          >
+            {isSidebarOpen ? <FiX size={24} /> : <FiMenu size={24} />}
+          </button>
 
-    return (
-        <nav className="navbar">
-            <div className="navbar-container">
-                {/* Left Section */}
-                <div className="navbar-left">
-                    <button
-                        onClick={onToggleSidebar}
-                        className="navbar-toggle"
-                        aria-label="Toggle menu"
-                    >
-                        {isSidebarOpen ? <FiX size={24} /> : <FiMenu size={24} />}
-                    </button>
-                    
-                    <Link to="/dashboard" className="navbar-logo">
-                        Mi Negocio
-                    </Link>
-                </div>
-
-                {/* Right Section */}
-                <div className="navbar-right">
-                    {user && (
-                        <>
-                            <span className="navbar-user-name">
-                                Hola, {user.name}
-                            </span>
-                            <button
-                                onClick={logout}
-                                className="navbar-logout-btn"
-                            >
-                                Cerrar sesión
-                            </button>
-                        </>
-                    )}
-                </div>
-            </div>
-        </nav>
-    );
+          <Link to="/dashboard" className="navbar-logo">
+            Mi Negocio
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
