255c3bea2354d6a6dd7e6ef6b97f25af717a7515
IanGorski <gorskiandevl@gmail.com>
2025-11-01 22:23:50 -0300

    feat(common): actualizar componente Card

--- frontend/src/components/common/Card.jsx
+++ frontend/src/components/common/Card.jsx
@@ -1,10 +1,15 @@
-import './Card.css';
+import "./Card.css";
 
-const Card = ({ title, children, className }) => {
+const Card = ({ title, children, className = "", subtitle }) => {
   return (
     <div className={`card ${className}`}>
-      {title && <h3 className="card-title">{title}</h3>}
-      {children}
+      {(title || subtitle) && (
+        <div className="card-header">
+          {title && <h3 className="card-title">{title}</h3>}
+          {subtitle && <p className="card-subtitle">{subtitle}</p>}
+        </div>
+      )}
+      <div className="card-content">{children}</div>
     </div>
   );
 };
